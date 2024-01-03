c599c2c8be38e289fcd8abca1e8b4539e37eb7ae
PyFra22 <locchifrancescoo@gmail.com>
2024-01-03 20:56:15 +0100

    Refactoring
    Refactoring del codice, creazione di diverse funzioni.

--- MainInterestRateo.java
+++ MainInterestRateo.java
@@ -2,23 +2,51 @@ import java.util.Scanner;
 
 // Interessi = (CapitaleInvestito*TassoInteresse * giorniTotali) / 36500
 
-public class Main {
+class Main {
 
-    static void interestCalculator(int assets, double rateoInterest){
+    static int eventualInputError(String repetingPhrase, int parameterToCheck){
+        int correctParameter = 0;
+        Scanner correctParameterInput = new Scanner(System.in);
+        correctParameter = correctParameterInput.nextInt();
+        if(parameterToCheck < 0){
+            do{
+                System.out.println("Hello World");
+            }while(Greta troia);
+        }
+        return correctParameter;
+    }
 
+    static int totalLeapYear(){
+        int leapYears;
+        Scanner leapYearsInput = new Scanner(System.in);
+        System.out.println("How many leap years?");
+        leapYears = leapYearsInput.nextInt();
+        if(leapYears < 0){
+            do{
+                System.out.println("Impossible amount of years");
+                leapYears = leapYearsInput.nextInt();
+            }while(leapYears < 0);
+        }
+        return leapYears;
+    }
+
+    static int totalDaysCalculator(){
         int totalDaysInYear;
         Scanner totalYearsScanner = new Scanner(System.in);
         totalDaysInYear = totalYearsScanner.nextInt();
-        totalDaysInYear = totalDaysInYear * 365;
+        totalDaysInYear = totalDaysInYear * 365 + totalLeapYear();
 
         if(totalDaysInYear < 0){
             do{
-                System.out.print("Impossible amount of years");
+                System.out.println("Impossible amount of years");
                 totalDaysInYear = totalYearsScanner.nextInt();
             }while(totalDaysInYear < 0);
         }
+        return totalDaysInYear;
+    }
 
-        double interests = (assets * rateoInterest * totalDaysInYear) / 36500;
+    static void daysAndInterestCalculator(int assets, double rateoInterest){
+        double interests = (assets * rateoInterest * totalDaysCalculator()) / 36500;
         System.out.println("You have a total of " + interests);
     }
 
@@ -36,16 +64,16 @@ public class Main {
             double annualInterestRate;
             Scanner interestRateInput = new Scanner(System.in);
             annualInterestRate = interestRateInput.nextDouble();
-            if (annualInterestRate > 0 && annualInterestRate < 30) {
+            if(annualInterestRate > 0 && annualInterestRate < 30) {
                 System.out.println("Insert now how many years");
-                interestCalculator(totalAmountOfMoney, annualInterestRate);
+                daysAndInterestCalculator(totalAmountOfMoney, annualInterestRate);
             }else{
                 do{
                     System.out.println("Insert the correct interest rate");
                     annualInterestRate = interestRateInput.nextFloat();
                 }while(annualInterestRate < 0 || annualInterestRate > 30);
                 System.out.println("Insert now how many years");
-                interestCalculator(totalAmountOfMoney, annualInterestRate);
+                daysAndInterestCalculator(totalAmountOfMoney, annualInterestRate);
             }
         }
     }
